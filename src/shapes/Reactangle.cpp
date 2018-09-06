e7642b7cdf098fd74360a39312473f42b22baf89
Alexander Yashkin <alex.aspirine@gmail.com>
2018-09-06 22:14:46 +0300

    Revert "Remove files for review"
    This reverts commit 584f0daf453a0b5db5966b9a6b05b2fd04b81025.

---
+++ src/shapes/Reactangle.cpp
@@ -0,0 +1,9 @@
+#include "Rectangle.h"
+
+std::string Rectangle::readData() const {
+    return "Rectangle ... data ..."; // return serializable data
+}
+
+void Rectangle::writeData(const std::string &){
+    // get inforamation from the string for the shape
+};
